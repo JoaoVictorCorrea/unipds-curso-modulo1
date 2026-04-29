a596e471acabd7d6e230e68b74a764a7df5e9b07
João Victor Correa <correa.joaov25@gmail.com>
2026-04-28 21:37:36 -0300

    Refactoring server to accept multithreading

--- src/BookServerWithSocket.java
+++ src/BookServerWithSocket.java
@@ -1,38 +1,44 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 void main() throws IOException {
     try (ServerSocket serverSocket = new ServerSocket(8000)){
         System.out.println("Book Server is running on port 8000...");
 
         while (true) {
-            try (Socket clientSocket = serverSocket.accept()) {
-                InputStream clientIS = clientSocket.getInputStream();
-
-                StringBuilder requestBuilder = new StringBuilder();
-                int data;
-
-                do {
-                    data = clientIS.read();
-                    requestBuilder.append((char) data);
-                } while (clientIS.available() > 0);
-
-                String request = requestBuilder.toString();
-                System.out.println(request);
-
-                Path path = Path.of("src/books.json");
-                System.out.println("Reading file: " + path.toAbsolutePath());
-                String json = Files.readString(path);
-
-                OutputStream clientOS = clientSocket.getOutputStream();
-                PrintStream clientOut = new PrintStream(clientOS);
-
-                clientOut.println("HTTP/1.1 200 OK");
-                clientOut.println("Content-Type: application/json; charset=UTF-8");
-                clientOut.println();
-                clientOut.println(json);
-            }
+            Socket clientSocket = serverSocket.accept();
+            Thread thread = new Thread(() -> processRequest(clientSocket));
+            thread.start();
         }
+    }
+}
+
+private static void processRequest(Socket clientSocket) {
+    try (clientSocket) {
+        InputStream clientIS = clientSocket.getInputStream();
+
+        StringBuilder requestBuilder = new StringBuilder();
+        int data;
+
+        do {
+            data = clientIS.read();
+            requestBuilder.append((char) data);
+        } while (clientIS.available() > 0);
+
+        String request = requestBuilder.toString();
+        System.out.println(request);
+
+        Thread.sleep(250);
+
+        Path path = Path.of("src/books.json");
+        System.out.println("Reading file: " + path.toAbsolutePath());
+        String json = Files.readString(path);
+
+        OutputStream clientOS = clientSocket.getOutputStream();
+        PrintStream clientOut = new PrintStream(clientOS);
+
+        clientOut.println("HTTP/1.1 200 OK");
+        clientOut.println("Content-Type: application/json; charset=UTF-8");
+        clientOut.println();
+        clientOut.println(json);
+    } catch (Exception e){
+        throw new RuntimeException(e);
     }
 }
